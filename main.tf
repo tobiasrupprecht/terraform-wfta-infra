1c9f3fafc273d53280f3c4163ad465c6f3ceca09
Tobias Rupprecht <trupprecht@hashicorp.com>
2024-11-14 13:43:53 +0100

    fixed K8s provider

--- main.tf
+++ main.tf
@@ -312,8 +312,8 @@ resource "aws_security_group" "eks_lb_sg" {
 
 # Kubernetes Provider
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster.certificate_authority.0.data)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
@@ -321,7 +321,7 @@ provider "kubernetes" {
       "eks",
       "get-token",
       "--cluster-name",
-      data.aws_eks_cluster.cluster.name
+      module.eks.cluster_name
     ]
   }
 }
