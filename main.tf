2ed0c6dbb8190c7d6b8ae9d6f9c399dfdd5a0eec
Tobias Rupprecht <trupprecht@hashicorp.com>
2024-11-14 18:06:27 +0100

    removed unnecessary EKS role

--- main.tf
+++ main.tf
@@ -99,41 +99,41 @@ resource "aws_iam_instance_profile" "ec2_profile" {
 ######### Unsure if I need the following section
 ####################################
 # EKS IAM Role and Policies
-resource "aws_iam_role" "eks_role" {
-  name = "eks_role"
+#resource "aws_iam_role" "eks_role" {
+#  name = "eks_role"
+#
+#  assume_role_policy = jsonencode({
+#    Version = "2012-10-17",
+#    Statement = [
+#      {
+#        Action = "sts:AssumeRole",
+#        Effect = "Allow",
+#        Principal = {
+#          Service = "eks.amazonaws.com"
+#        }
+#      }
+#    ]
+#  })
+#}
+#
+#resource "aws_iam_role_policy_attachment" "eks_policy_attachment" {
+#  role       = aws_iam_role.eks_role.name
+#  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+#}
+#
+#resource "aws_iam_role_policy_attachment" "eks_service_policy_attachment" {
+#  role       = aws_iam_role.eks_role.name
+#  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+#}
+#
+#resource "aws_iam_role_policy_attachment" "eks_vpc_policy_attachment" {
+#  role       = aws_iam_role.eks_role.name
+#  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+#}
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eks_policy_attachment" {
-  role       = aws_iam_role.eks_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_service_policy_attachment" {
-  role       = aws_iam_role.eks_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_vpc_policy_attachment" {
-  role       = aws_iam_role.eks_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-}
-
-resource "aws_iam_instance_profile" "eks_instance_profile" {
-  role = aws_iam_role.eks_role.name
-}
+#resource "aws_iam_instance_profile" "eks_instance_profile" {
+#  role = aws_iam_role.eks_role.name
+#}
 #################### Unsure if I need the section above
 #############################################################
 
