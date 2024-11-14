3bd995ab30060bd90834c618a73afa6016aa5dd7
Tobias Rupprecht <trupprecht@hashicorp.com>
2024-11-14 13:46:29 +0100

    fixed cluster_ca_certificate

--- main.tf
+++ main.tf
@@ -313,7 +313,7 @@ resource "aws_security_group" "eks_lb_sg" {
 # Kubernetes Provider
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster.certificate_authority.0.data)
+  cluster_ca_certificate = base64decode(module.eks.certificate_authority.0.data)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
