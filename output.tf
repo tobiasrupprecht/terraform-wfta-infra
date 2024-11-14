0b55596f67e9404c77f31d8f029c7fc9773e498f
Tobias Rupprecht <trupprecht@hashicorp.com>
2024-11-14 22:12:37 +0100

    new image version and more outputs

--- output.tf
+++ output.tf
@@ -25,3 +25,7 @@ output "load_balancer_address" {
   description = "Public IP of the LoadBalancer service"
   value       = kubernetes_service.web_app_lb.status[0].load_balancer[0].ingress[0].ip
 }
+
+output "load_balancer_hostname" {
+  value = kubernetes_service.web_app_lb.status.0.load_balancer.0.ingress.0.hostname
+}
