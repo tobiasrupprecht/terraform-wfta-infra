6e96a2fadfa34dfc7e13ad011434278b7847caa2
Tobias Rupprecht <trupprecht@hashicorp.com>
2024-11-15 15:51:33 +0100

    MONGODB_URI

--- main.tf
+++ main.tf
@@ -407,7 +407,7 @@ resource "kubernetes_deployment" "web_app_deployment" {
           }
           env {
             name  = "MONGODB_URI"
-            value = "mongodb://admin:password@${aws_instance.database_server.private_ip}:27017/?ssl=false&authSource=admin"
+            value = "mongodb://admin:password@${aws_instance.database_server.private_ip}:27017/?tls=false&authSource=admin"
           }
           env {
             name  = "SECRET_KEY"
