7432491a9d2c776501e34750824eccbdd51c8854
Tobias Rupprecht <trupprecht@hashicorp.com>
2024-11-15 12:48:33 +0100

    canged cidr of security group for DB EC2

--- main.tf
+++ main.tf
@@ -88,7 +88,8 @@ resource "aws_security_group" "database_sg" {
     from_port   = 27017
     to_port     = 27017
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+  # cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   # Allow SSH from the public internet
